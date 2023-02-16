3229558fba75533649780915d00cb70b49b6d500
pingspong <pingspong@outlook.com>
2023-02-16 11:52:45 +0800

    Initial commit

---
+++ src/main/java/com/apollo/apolloapi/ApolloApiApplication.java
@@ -0,0 +1,13 @@
+package com.apollo.apolloapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApolloApiApplication.class, args);
+	}
+
+}
